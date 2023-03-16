447446eb24796dd6b7146cdadd2cf6d368a46594
Jenny <jenny.rocha.mashka@gmail.com>
2023-03-16 10:07:51 -0500

    FormBuilder

--- src/app/characters/form-character/form-character.component.ts
+++ src/app/characters/form-character/form-character.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Character } from '../character';
 import { CharacterService } from '../character.service';
 import { Routes, RouterModule, Router, ActivatedRoute } from '@angular/router';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup ,FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-form-character',
@@ -10,6 +10,7 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
   styleUrls: ['./form-character.component.css']
 })
 export class FormCharacterComponent implements OnInit {
+  resultado!: string;
   character: Character = new Character();
   imageFormcontrol: FormControl = new FormControl();
   control: FormControl = new FormControl();
@@ -26,7 +27,7 @@ export class FormCharacterComponent implements OnInit {
     homeworld: new FormControl('')
   });
 
-  constructor(private characterService:CharacterService, private router:Router, private activateRoute:ActivatedRoute) {
+  constructor(private characterService:CharacterService, private router:Router, private activateRoute:ActivatedRoute, private fb: FormBuilder) {
 
     this.control = new FormControl('', [
       Validators.required,      // Es requerido
@@ -40,7 +41,15 @@ export class FormCharacterComponent implements OnInit {
       Validators.pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)   //Validamos que sea una url correcta.
     ]);  
   }
-
+  formularioContacto = this.fb.group({
+    birthyear: ['', [Validators.required]]// Es requerido
+  });
+  submit() {
+    if (this.formularioContacto.valid)
+      this.resultado = "Todos los datos son válidos";
+    else
+      this.resultado = "Hay datos inválidos en el formulario";
+  }
   ngOnInit(): void {
     this.getCharacterById();
     console.log(this.imageFormcontrol);
